2445cac04ac35ee1c8c90b145b4051e6d1101536
Rasmus Nielsen <rasmus@easify.dk>
2018-01-29 14:12:58 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/Stubs/UserRegistered.php
+++ tests/Stubs/UserRegistered.php
@@ -2,9 +2,9 @@
 
 namespace Makeable\LaravelEssentials\Tests\Stubs;
 
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
 
 class UserRegistered
 {
