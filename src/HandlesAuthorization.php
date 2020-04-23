d69a684493200097a1d136900068f44134c16624
Rasmus Nielsen <rasmus@easify.dk>
2020-04-23 11:10:29 +0200

    Fix abort helpers for Laravel 6+

--- src/HandlesAuthorization.php
+++ src/HandlesAuthorization.php
@@ -7,30 +7,26 @@ trait HandlesAuthorization
     use \Illuminate\Auth\Access\HandlesAuthorization;
 
     /**
-     * @param $condition
+     * @param  mixed  $condition
      * @param  string  $message
      * @return void
      *
-     * @throws \Illuminate\Auth\Access\AuthorizationException
+     * @throws \Illuminate\Http\Exceptions\HttpResponseException
      */
     protected function denyIf($condition, $message = null)
     {
-        if ($condition) {
-            $this->deny($message);
-        }
+        abort_if($condition, $this->deny($message));
     }
 
     /**
-     * @param $condition
+     * @param  mixed  $condition
      * @param  string  $message
      * @return void
      *
-     * @throws \Illuminate\Auth\Access\AuthorizationException
+     * @throws \Illuminate\Http\Exceptions\HttpResponseException
      */
     protected function denyUnless($condition, $message = null)
     {
-        if (! $condition) {
-            $this->deny($message);
-        }
+        abort_unless($condition, $this->deny($message));
     }
 }
